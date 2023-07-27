e1a38665fe775b562837ba7a9a0e500f8717f8eb
JadenCooper <jaden_cooper@outlook.com>
2023-07-27 23:19:42 +1200

    Changed Movment From Joystick To Buttons

--- IdleCat/Assets/Scripts/AgentMover.cs
+++ IdleCat/Assets/Scripts/AgentMover.cs
@@ -9,17 +9,9 @@ public class AgentMover : MonoBehaviour
     private Vector2 movement;
     private Animator animator;
     public Rigidbody2D rb2d;
-    public float currentSpeed;
-    public float Acceleration;
     public float MaxSpeed;
     private bool isMoving;
     private SpriteRenderer spriteRenderer;
-    public bool isJumping = false;
-
-    public float jumpPower = 15;
-    public bool isGrounded = true;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
     private void Awake()
     {
         rb2d = GetComponentInParent<Rigidbody2D>();
@@ -42,7 +34,6 @@ public class AgentMover : MonoBehaviour
         }
         else
         {
-            currentSpeed += Acceleration * Time.deltaTime;
             isMoving = true;
             CheckSide();
         }
@@ -61,30 +52,7 @@ public class AgentMover : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (!isJumping)
-        {
-            if (movement.y >= 0.5)
-            {
-                isJumping = true;
-                movement.y = jumpPower;
-                rb2d.velocity = movement * Time.deltaTime;
-            }
-            else
-            {
-                movement.y = 0;
-                animator.SetBool("isMoving", isMoving);
-            }
-            rb2d.velocity = movement * Time.deltaTime;
-        }
-        else
-        {
-            isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.24f, 0.02f), CapsuleDirection2D.Horizontal, 0, groundLayer);
-            Debug.Log("Check");
-            if (isGrounded)
-            {
-                Debug.Log("Grounded");
-                isJumping = false;
-            }
-        }
+        rb2d.velocity = movement * Time.deltaTime;
+        animator.SetBool("isMoving", isMoving);
     }
 }
