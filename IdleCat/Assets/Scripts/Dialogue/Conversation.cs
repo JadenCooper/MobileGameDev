284fadd7ec70a3b47e895a7d92dd1e89cd7b87f0
JadenCooper <jaden_cooper@outlook.com>
2023-09-18 14:36:15 +1200

    Created UI Palette

--- IdleCat/Assets/Scripts/Dialogue/Conversation.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewConversation", menuName = "Data/Conversation")]
-public class Conversation : ScriptableObject
-{
-    public List<Dialogue> dialogues = new List<Dialogue>();
-}
