838b473ef39ed5a13c2e088d6cd0c5bebf0810e1
JadenCooper <jaden_cooper@outlook.com>
2023-09-18 18:40:31 +1200

    Created Flexiable Grid Layout And Tab System

---
+++ IdleCat/Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using TMPro;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+    public Image  background;
+    public TMP_Text text;
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+    private void Start()
+    {
+        background = GetComponent<Image>();
+        text = GetComponentInChildren<TMP_Text>();
+        tabGroup.Subscribe(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void Select()
+    {
+        if (onTabSelected != null)
+        {
+            onTabSelected.Invoke();
+        }
+    }
+
+    public void Deselect()
+    {
+        if (onTabDeselected != null)
+        {
+            onTabDeselected.Invoke();
+        }
+    }
+
+}
